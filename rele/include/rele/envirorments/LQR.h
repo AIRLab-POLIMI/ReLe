1c8c0d2f74be39af74f54f2491889d8c74b99954
Matteo <matteo.pirotta@polimi.it>
2015-02-09 18:07:02 +0100

    Added ContinuousMDP and LQR

---
+++ rele/include/rele/envirorments/LQR.h
@@ -0,0 +1,54 @@
+/*
+ * rele,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo & Matteo Pirotta
+ * Versione 1.0
+ *
+ * This file is part of rele.
+ *
+ * rele is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rele is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rele.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LQR_H_
+#define LQR_H_
+
+#include "ContinuousMDP.h"
+
+namespace ReLe
+{
+
+class LQR: public ContinuousMDP
+{
+public:
+    LQR(unsigned int dimension, unsigned int reward_dimension,
+        double eps = 0.1, double gamma = 0.99, unsigned int horizon = 100);
+    LQR(arma::mat& A, arma::mat& B, std::vector<arma::mat>& Q, std::vector<arma::mat>& R,
+        double gamma = 0.99, unsigned int horizon = 100);
+    virtual void step(const DenseAction& action, DenseState& nextState,
+                      Reward& reward);
+    virtual void getInitialState(DenseState& state);
+
+private:
+    void initialize(unsigned int stateActionSize, unsigned int rewardSize, double e);
+
+private:
+    arma::mat A, B;
+    std::vector<arma::mat> Q, R;
+
+};
+
+}
+
+#endif /* LQR_H_ */
---
+++ rele/src/envirorments/LQR.cpp
@@ -0,0 +1,96 @@
+/*
+ * rele,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo & Matteo Pirotta
+ * Versione 1.0
+ *
+ * This file is part of rele.
+ *
+ * rele is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rele is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rele.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "LQR.h"
+#include "RandomGenerator.h"
+#include <cassert>
+
+using namespace std;
+
+namespace ReLe
+{
+
+LQR::LQR(unsigned int dimension, unsigned int reward_dimension, double eps, double gamma, unsigned int horizon) :
+    ContinuousMDP(dimension, dimension, reward_dimension, true, true, gamma, horizon),
+    A(dimension,dimension), B(dimension,dimension)
+{
+    initialize(dimension, reward_dimension, eps);
+}
+
+LQR::LQR(arma::mat &A, arma::mat &B, std::vector<arma::mat> &Q, std::vector<arma::mat> &R, double gamma, unsigned int horizon) :
+    ContinuousMDP(A.n_cols, B.n_cols, Q.size(), true, true, gamma, horizon),
+    A(A), B(B), Q(Q), R(R)
+{
+    assert(Q.size() == R.size());
+}
+
+void LQR::step(const DenseAction& action,
+                       DenseState& nextState, Reward& reward)
+{
+    for (unsigned int i = 0, ie = Q.size(); i < ie; ++i) {
+        reward[i] = -(currentState.t()*Q[i]*currentState + action.t()*R[i]*action);
+    }
+    currentState = A*currentstate + B*action;
+
+    nextState = currentState;
+}
+
+void LQR::getInitialState(DenseState& state)
+{
+    for (unsigned int i = 0, ie = Q.size(); i < ie; ++i) {
+        currentState[i] = RandomGenerator::sampleUniform(-20,20);
+    }
+    currentState.setAbsorbing(false);
+
+    state = currentState;
+}
+
+void LQR::initialize(unsigned int stateActionSize, unsigned int rewardSize, double e)
+{
+    A.eye();
+    B.eye();
+
+    arma::mat IdMtx(stateActionSize, stateActionSize);
+    IdMtx.eye();
+
+    for (int i = 0, ie = rewardSize; i < ie; ++i) {
+        Q.push_back(IdMtx);
+        R.push_back(IdMtx);
+    }
+
+    for (int i = 0, ie = stateActionSize; i < ie; ++i) {
+        for (int j = 0, je = stateActionSize; j < je; ++j) {
+
+            if (i == j) {
+                Q[i](j,j) = 1.0 - e;
+                R[i](j,j) = e;
+            } else {
+                Q[i](j,j) = e;
+                R[i](j,j) = 1.0 - e;
+            }
+        }
+    }
+
+}
+
+}
