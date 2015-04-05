10a083cf2b7f5e8673eca4be50640b74aca22d57
Matteo <matteo.pirotta@polimi.it>
2015-04-05 19:34:05 +0200

    Updated

--- rele_matlab/src/Statistics/ReadREPSStatistics.m
+++ rele_matlab/src/Statistics/ReadREPSStatistics.m
@@ -3,15 +3,21 @@ function [ iteration, index ] = ReadREPSStatistics(csv, index)
 %   Read all the parameters used by the algorithm and their reward
 
 index = index +1; %skip episode info
-iteration.metaParams = csv(index, :);
+nbparameters = csv(index, 1);
+index = index +1;
+iteration.metaParams = csv(index, 1:nbparameters);
+index = index +1;
+nbPolParams = csv(index, 1);
+index = index +1;
+nbEpisodesPerPolicy = csv(index, 1);
 index = index +1; 
 individualsN = csv(index, 1);
     
 for i=1:individualsN
     index = index +1;
-    iteration.policies(i).policy = csv(index, :);
+    iteration.policies(i).policy = csv(index, 1:nbPolParams);
     index = index +1;
-    iteration.policies(i).J = csv(index, 1);
+    iteration.policies(i).J = csv(index, 1:nbEpisodesPerPolicy);
 end
 
 index = index + 1;
