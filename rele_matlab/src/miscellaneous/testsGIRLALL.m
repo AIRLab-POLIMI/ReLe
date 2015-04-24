f1180bb46635ec95f5ab994ac190655a4a45e0a2
Matteo <matteo.pirotta@polimi.it>
2015-04-24 19:41:45 +0200

    Updated

--- rele_matlab/src/miscellaneous/testsGIRLALL.m
+++ rele_matlab/src/miscellaneous/testsGIRLALL.m
@@ -71,9 +71,9 @@ for k = 6:size(W,1)
                 plane_T = A(2,:);
                 clear A;
                 results(i).plane{kk} = [results(i).plane{kk};plane_R];
-                results(i).plane_time{kk} = [results(i).time{kk};plane_T];
+                results(i).plane_time{kk} = [results(i).plane_time{kk};plane_T];
                 results(i).gnorm{kk} = [results(i).gnorm{kk};gnorm_R];
-                results(i).gnorm_time{kk} = [results(i).time{kk};gnorm_T];
+                results(i).gnorm_time{kk} = [results(i).gnorm_time{kk};gnorm_T];
             end
             
             
@@ -87,3 +87,142 @@ for k = 6:size(W,1)
 end
 
 save test2.mat
+
+%%
+clc;
+dimr = length(eReward);
+aa = repmat('c', 1, dimr);
+nalg = length(gtypes);
+ftab = ['{c', repmat(['|',aa],1, nalg*2), '}'];
+ftabtime = ['{c', repmat('|c',1, nalg*2), '}'];
+clear aa;
+clear i;
+for i = 1:length(test)
+    fprintf('\\begin{table}\n\\begin{small}\n\\centering\n\\begin{tabular}');
+    fprintf('%s\n', ftab);
+    fprintf('\\hline \n');
+    fprintf('\\multirow{2}{*}{Episodes} & \\multicolumn{%d}{c|}{PGIRL} & \\multicolumn{%d}{c}{GIRL}\\\\ \n',nalg*dimr,nalg*dimr);
+    fprintf('& \\multicolumn{%d}{c|}{R} & \\multicolumn{%d}{c|}{RB}', dimr, dimr);
+    fprintf('& \\multicolumn{%d}{c|}{G} & \\multicolumn{%d}{c|}{GB} ', dimr, dimr);
+    fprintf('& \\multicolumn{%d}{c|}{R} & \\multicolumn{%d}{c|}{RB}', dimr, dimr);
+    fprintf('& \\multicolumn{%d}{c|}{G} & \\multicolumn{%d}{c}{GB} \\\\ \n', dimr, dimr);
+    fprintf('\\hline \n');
+    for epel = 1:length(test(i).results)
+        fprintf('\\multirow{2}{*}{$%d$} ', test(i).results(epel).ep);
+        for alg = 1:length(test(i).results(epel).plane)
+            valplane = test(i).results(epel).plane{alg};
+            timeplane = test(i).results(epel).plane_time{alg};
+            valgnorm = test(i).results(epel).gnorm{alg};
+            timegnorm = test(i).results(epel).gnorm_time{alg};
+            nruns = size(valplane,1);
+            
+            muplane = mean(valplane);
+            mugnorm = mean(valgnorm);
+            stdplane = std(valplane) / sqrt(nruns-1);
+            stdgnorm = std(valgnorm) / sqrt(nruns-1);
+            mutimeplane = mean(timeplane);
+            mutimegnorm = mean(timegnorm);
+            
+            for nel = 1:size(valplane,2)
+                fprintf('& %.3f ', muplane(nel));
+            end
+            
+            
+            for nel = 1:size(valplane,2)
+                fprintf('& %.3f ', mugnorm(nel));
+            end
+        end
+        fprintf('\\\\ \n');
+        % stddev
+        for alg = 1:length(test(i).results(epel).plane)
+            valplane = test(i).results(epel).plane{alg};
+            timeplane = test(i).results(epel).plane_time{alg};
+            valgnorm = test(i).results(epel).gnorm{alg};
+            timegnorm = test(i).results(epel).gnorm_time{alg};
+            nruns = size(valplane,1);
+            
+            muplane = mean(valplane);
+            mugnorm = mean(valgnorm);
+            stdplane = std(valplane) / sqrt(nruns-1);
+            stdgnorm = std(valgnorm) / sqrt(nruns-1);
+            mutimeplane = mean(timeplane);
+            mutimegnorm = mean(timegnorm);
+            
+            for nel = 1:size(valplane,2)
+                fprintf('& {\\scriptsize $\\pm %.3f$} ', stdplane(nel));
+            end
+            
+            
+            for nel = 1:size(valplane,2)
+                fprintf('& {\\scriptsize $\\pm %.3f$} ', stdgnorm(nel));
+            end
+        end
+        fprintf('\\\\ \n');
+        fprintf('\\hline \n');
+    end
+    fprintf('\\end{tabular} \n');
+    strval = '';
+    rew = test(i).rew;
+    for oo = 1:dimr
+        strval = [strval, ' ', num2str(rew(oo))];
+    end
+    fprintf('\\caption{Weights recovered starting form [%s]}',strval);
+    fprintf('\\end{small}\n');
+    fprintf('\\end{table}\n');
+    
+    %% TIME
+    fprintf('\\begin{table}\n\\begin{small}\n\\centering\n\\begin{tabular}');
+    fprintf('%s\n', ftabtime);
+    fprintf('\\hline \n');
+    fprintf('\\multirow{2}{*}{Episodes} & \\multicolumn{%d}{c|}{PGIRL} & \\multicolumn{%d}{c}{GIRL}\\\\ \n',nalg,nalg);
+    fprintf('& R & RB & G & BG & R & RB & G & BG \\\\ \n');
+    fprintf('\\hline \n');
+    for epel = 1:length(test(i).results)
+        fprintf('\\multirow{1}{*}{$%d$} ', test(i).results(epel).ep);
+        for alg = 1:length(test(i).results(epel).plane)
+            timeplane = test(i).results(epel).plane_time{alg};
+            timegnorm = test(i).results(epel).gnorm_time{alg};
+            nruns = size(timeplane,1);
+            
+            muplane = mean(timeplane);
+            mugnorm = mean(timegnorm);
+            stdplane = std(timeplane) / sqrt(nruns-1);
+            stdgnorm = std(timegnorm) / sqrt(nruns-1);
+            
+            fprintf('& %.3f ', muplane);
+            
+            
+            fprintf('& %.3f ', mugnorm);
+            
+        end
+        fprintf('\\\\ \n');
+%         % stddev
+%         for alg = 1:length(test(i).results(epel).plane)
+%             timeplane = test(i).results(epel).plane_time{alg};
+%             timegnorm = test(i).results(epel).gnorm_time{alg};
+%             nruns = size(timeplane,1);
+%             
+%             muplane = mean(timeplane);
+%             mugnorm = mean(timegnorm);
+%             stdplane = std(timeplane) / sqrt(nruns-1);
+%             stdgnorm = std(timegnorm) / sqrt(nruns-1);
+%             
+%                 fprintf('& {\\scriptsize $\\pm %.3f$} ', stdplane);
+%             
+%                 fprintf('& {\\scriptsize $\\pm %.3f$} ', stdgnorm);
+%             
+%         end
+%         fprintf('\\\\ \n');
+        fprintf('\\hline \n');
+    end
+    fprintf('\\end{tabular} \n');
+    strval = '';
+    rew = test(i).rew;
+    for oo = 1:dimr
+        strval = [strval, ' ', num2str(rew(oo))];
+    end
+    fprintf('\\caption{Times (s) [%s], non sono mediati perche'' salvavo solo l''ultimo (errore nello script)}',strval);
+    fprintf('\\end{small}\n');
+    fprintf('\\end{table}\n');
+    fprintf('\\clearpage\n');
+end
