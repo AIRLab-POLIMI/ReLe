585e574e457ae8b139fc3ba48fb995f6461269b1
Boris_il_forte <davide.tateo90@gmail.com>
2015-09-28 17:03:22 +0200

    Matlab stuff

--- rele_matlab/src/tests/IRL/EMIRL/printLQRTraj.m
+++ rele_matlab/src/tests/IRL/EMIRL/printLQRTraj.m
@@ -56,7 +56,7 @@ disp(theta(:, all(theta > 0, 1)))
 phi = load('/tmp/ReLe/lqr/EMIRL/Phi.txt' , '-ascii');
 figure(4)
 hold on;
-inliers = any(phi > -1e3);
+inliers = all(phi > -1000);
 plot3(phi(1, inliers), phi(2, inliers), phi(3, inliers), 'ob');
 
 disp('----------------')
@@ -64,4 +64,4 @@ disp('inlier features ratio:')
 disp(sum(inliers)/length(phi));
 
 disp('outliers features:')
-disp(sum(inliers))
+disp(length(phi)-sum(inliers))
