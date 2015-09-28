f683c5726a8da578518418790f0ea493a686bc1c
Boris_il_forte <davide.tateo90@gmail.com>
2015-09-28 12:01:54 +0200

    Work on visualization of LQR EMIRL results
    - some results prints on lqr settings
    - stuff for analyzing data

--- rele_matlab/src/graphics/plotPlane.m
+++ rele_matlab/src/graphics/plotPlane.m
@@ -1,12 +1,29 @@
-function plotPlane( xgv, ygv, pi)
+function plotPlane(p, n, tgv, sgv)
 %PLOTPLANE plot a plane
-%   TO BE COMPLETED
+%   Given a normal vector and a point, plots a plane. 
 
-[X,Y] = meshgrid(xgv, ygv);
-a=2; b=-3; c=10; d=-1;
-Z=(d- a * X - b * Y)/c;
-surf(X,Y,Z)
-shading flat
+[T,S] = meshgrid(tgv, sgv);
+
+b = eye(3);
+
+for i=1:3
+    tmp = cross(n, b(:, i));  
+    if(any(tmp))
+        break;
+    end
+end
+
+v1 = cross(tmp, n);
+v1 = v1/norm(v1);
+v2 = cross(v1, n);
+v2 = v2/norm(v2);
+
+X = p(1) + T*v1(1) + S*v2(1); 
+Y = p(2) + T*v1(2) + S*v2(2);
+Z = p(3) + T*v1(3) + S*v2(3);
+
+
+surf(X,Y,Z, 'FaceColor','None')
 
 end
 
