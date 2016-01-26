67a030e8b8b0508ffb08deb439c26e96d00f1014
carloderamo <carlo.deramo@gmail.com>
2016-01-26 12:59:29 +0100

    Correction to minimum value and infinite value for sigma in WQ-Learning.

--- rele/include/rele/algorithms/td/WQ-Learning.h
+++ rele/include/rele/algorithms/td/WQ-Learning.h
@@ -33,8 +33,8 @@
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_cdf.h>
 
-#define STD_ZERO_VALUE 1E-100
-#define STD_INF_VALUE 1E100
+#define STD_ZERO_VALUE 1E-10
+#define STD_INF_VALUE 1E10
 
 
 /*
