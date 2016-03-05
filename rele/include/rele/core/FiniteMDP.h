4dee0a6b8a13e1ffd8b643a7aa5974f566da7173
Boris_il_forte <davide.tateo90@gmail.com>
2016-03-05 23:21:38 +0100

    Trying to fix doc compilation

--- rele/include/rele/core/FiniteMDP.h
+++ rele/include/rele/core/FiniteMDP.h
@@ -68,15 +68,9 @@ public:
      */
     FiniteMDP(arma::cube P, arma::cube R, arma::cube Rsigma, EnvironmentSettings* settings);
 
-    /*!
-     * \see Environment::step
-     */
     virtual void step(const FiniteAction& action, FiniteState& nextState,
                       Reward& reward) override;
 
-    /*!
-     * \see Environment::getInitialState
-     */
     virtual void getInitialState(FiniteState& state) override;
 
 private:
