eb51f02aff4f3fb2473249d8c2c3c8db39b874b9
boris-il-forte <boris.ilpossente@hotmail.it>
2016-03-23 10:49:19 +0100

    Added automatic build of tutorials
    - Now tutorials are build with the doc

---
+++ rele/doc/tutorials/code/q_learning.cpp
@@ -0,0 +1,40 @@
+#include "rele/core/FiniteMDP.h"
+#include "rele/algorithms/td/SARSA.h"
+#include "rele/algorithms/td/Q-Learning.h"
+#include "rele/core/Core.h"
+
+#include "rele/policy/q_policy/e_Greedy.h"
+#include "rele/policy/q_policy/Boltzmann.h"
+
+#include "rele/generators/SimpleChainGenerator.h"
+
+#include "rele/approximators/basis/IdentityBasis.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace ReLe;
+
+int main(int argc, char *argv[])
+{
+    SimpleChainGenerator generator;
+    generator.generate(5, 2);
+
+    FiniteMDP mdp = generator.getMDP(0.9);
+    e_Greedy policy;
+    ConstantLearningRate alpha(0.2);
+
+    Q_Learning agent(policy, alpha);
+
+    Core<FiniteAction, FiniteState> core(mdp, agent);
+
+    core.getSettings().episodeLength = 10000;
+    bool logTransition = false;
+    bool logAgent = true;
+    core.getSettings().loggerStrategy = new PrintStrategy<FiniteAction, FiniteState>(logTransition, logAgent);
+
+    cout << "starting episode" << endl;
+    core.runEpisode();
+    delete core.getSettings().loggerStrategy;
+
+}
