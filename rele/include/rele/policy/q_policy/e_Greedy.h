87e26dcc48366af64bf6581afd86e48b3208a85a
carloderamo <carlo.deramo@gmail.com>
2016-04-25 16:42:46 +0200

    fixes

--- rele/include/rele/policy/q_policy/e_Greedy.h
+++ rele/include/rele/policy/q_policy/e_Greedy.h
@@ -98,5 +98,47 @@ protected:
 
 };
 
+class e_GreedyMultipleRegressors: public ActionValuePolicy<DenseState>
+{
+public:
+    e_GreedyMultipleRegressors(std::vector<std::vector<BatchRegressor*>>& regressors);
+    virtual ~e_GreedyMultipleRegressors();
+
+    virtual unsigned int operator()(const arma::vec& state) override;
+    virtual double operator()(const arma::vec& state, const unsigned int& action) override;
+
+    virtual hyperparameters_map getPolicyHyperparameters() override;
+
+    inline void setEpsilon(double eps)
+    {
+        this->eps = eps;
+    }
+
+    inline virtual std::string getPolicyName() override
+    {
+        return "MultipleRegressors e-Greedy";
+    }
+
+    inline void setRegressor(std::vector<std::vector<BatchRegressor*>>& regressors)
+    {
+        this->regressors = regressors;
+    }
+
+    inline double getEpsilon()
+    {
+        return this->eps;
+    }
+
+    virtual e_GreedyMultipleRegressors* clone() override
+    {
+        return new e_GreedyMultipleRegressors(*this);
+    }
+
+protected:
+    double eps;
+    std::vector<std::vector<BatchRegressor*>>& regressors;
+};
+
 }
+
 #endif /* E_GREEDY_H_ */
--- rele/src/policy/q_policy/e_Greedy.cpp
+++ rele/src/policy/q_policy/e_Greedy.cpp
@@ -164,4 +164,82 @@ hyperparameters_map e_GreedyApproximate::getPolicyHyperparameters()
     return hyperParameters;
 }
 
+e_GreedyMultipleRegressors::e_GreedyMultipleRegressors(std::vector<std::vector<BatchRegressor*>>& regressors) :
+    regressors(regressors),
+    eps(eps)
+{
+}
+
+e_GreedyMultipleRegressors::~e_GreedyMultipleRegressors()
+{
+}
+
+unsigned int e_GreedyMultipleRegressors::operator()(const arma::vec& state)
+{
+    unsigned int un;
+
+    /*epsilon--greedy policy*/
+    if (RandomGenerator::sampleEvent(this->eps))
+        un = RandomGenerator::sampleUniformInt(0, nactions - 1);
+    else
+    {
+        unsigned int nstates = state.size();
+        vec regInput(nstates);
+        regInput = state;
+        un = 0;
+
+        arma::vec outputs(regressors.size(), arma::fill::zeros);
+        for(unsigned int r = 0; r < regressors.size(); r++)
+        {
+        	auto& self = *regressors[r][un];
+        	outputs(r) = self(regInput)[0];
+        }
+
+        double qmax = arma::as_scalar(arma::mean(outputs));
+        std::vector<int> optimal_actions;
+        optimal_actions.push_back(un);
+        for(unsigned int i = 1; i < nactions; ++i)
+        {
+            arma::vec outputs(regressors.size(), arma::fill::zeros);
+            for(unsigned int r = 0; r < regressors.size(); r++)
+            {
+            	auto& self = *regressors[r][i];
+            	outputs(r) = self(regInput)[0];
+            }
+
+            double qvalue = arma::as_scalar(arma::mean(outputs));
+            if (qmax < qvalue)
+            {
+                optimal_actions.clear();
+                qmax = qvalue;
+                un = i;
+                optimal_actions.push_back(un);
+            }
+            else if (qmax == qvalue)
+            {
+                optimal_actions.push_back(i);
+            }
+        }
+        unsigned int index = RandomGenerator::sampleUniformInt(0,
+                             optimal_actions.size() - 1);
+        un = optimal_actions[index];
+    }
+
+    return un;
+}
+
+double e_GreedyMultipleRegressors::operator()(const arma::vec& state, const unsigned int& action)
+{
+    //TODO [IMPORTANT] implement
+    assert(false);
+    return 0.0;
+}
+
+hyperparameters_map e_GreedyMultipleRegressors::getPolicyHyperparameters()
+{
+    hyperparameters_map hyperParameters;
+    hyperParameters["eps"] = eps;
+    return hyperParameters;
+}
+
 }
