769f9660a5c481f952498330b63a7a14c72467b0
carloderamo <carlo.deramo@gmail.com>
2016-04-28 21:22:45 +0200

    updated fqi gp policy test

--- rele/test/Batch/GP-FQIPolicyTest.cpp
+++ rele/test/Batch/GP-FQIPolicyTest.cpp
@@ -161,22 +161,34 @@ int main(int argc, char *argv[])
     policy.setNactions(nActions);
     PolicyEvalAgent<FiniteAction, DenseState> agent(policy);
 
-    unsigned int counter = 1;
-    for(int i = -8; i <= 8; i++)
-        for(int j = -8; j <= 8; j++)
-        {
-            double initialPosition = 0.125 * i;
-            double initialVelocity = 0.375 * j;
-            MountainCar testMdp(MountainCar::Ernst, initialPosition, initialVelocity);
-            auto&& core = buildCore(testMdp, agent);
-            core.getSettings().episodeLength = 10000;
-            core.getSettings().loggerStrategy =
-                new WriteStrategy<FiniteAction, DenseState>(fm.addPath(fileName));
+    if(env == "mc")
+    {
+		unsigned int counter = 1;
+		for(int i = -8; i <= 8; i++)
+			for(int j = -8; j <= 8; j++)
+			{
+				double initialPosition = 0.125 * i;
+				double initialVelocity = 0.375 * j;
+				MountainCar testMdp(MountainCar::Ernst, initialPosition, initialVelocity);
+				auto&& core = buildCore(testMdp, agent);
+				core.getSettings().episodeLength = 10000;
+				core.getSettings().loggerStrategy =
+					new WriteStrategy<FiniteAction, DenseState>(fm.addPath(fileName));
 
-            core.runTestEpisode();
+				core.runTestEpisode();
 
-            std::cout << counter++ << "/289" << std::endl;
-        }
+				std::cout << counter++ << "/289" << std::endl;
+			}
 
-    delete mdp;
+		delete mdp;
+    }
+    else if(env == "ip")
+    {
+		auto&& core = buildCore(*mdp, agent);
+		core.getSettings().episodeLength = 10000;
+		core.getSettings().loggerStrategy =
+			new WriteStrategy<FiniteAction, DenseState>(fm.addPath(fileName));
+
+		core.runTestEpisode();
+    }
 }
