8b1a8e86f3706553b2053c7c51c215267fb750cb
boris-il-forte <boris.ilpossente@hotmail.it>
2016-05-09 17:46:58 +0200

    Added Roundoff check

--- rele/include/rele/IRL/algorithms/LinearIRLAlgorithm.h
+++ rele/include/rele/IRL/algorithms/LinearIRLAlgorithm.h
@@ -92,17 +92,31 @@ public:
 
         //optimize function
         double minf;
-        if (optimizator.optimize(parameters, minf) < 0)
+
+        try
         {
-            std::cout << "nlopt failed!" << std::endl;
+			nlopt::result result = optimizator.optimize(parameters, minf);
+
+			if (result < 0)
+			{
+				std::cout << "nlopt failed!" << std::endl;
+				arma::vec x = simplex.getCenter();
+				rewardf.setParameters(x);
+			}
+			else
+			{
+				std::cout << "found minimum = " << minf << std::endl;
+
+
+				arma::vec x = simplex.reconstruct(parameters);
+				rewardf.setParameters(x);
+			}
         }
-        else
+        catch(nlopt::roundoff_limited& e)
         {
-            std::cout << "found minimum = " << minf << std::endl;
-
-
-            arma::vec x = simplex.reconstruct(parameters);
-            rewardf.setParameters(x);
+        	std::cout << "WARNING, roundoff limited!!!" << std::endl;
+        	arma::vec x = simplex.reconstruct(parameters);
+        	rewardf.setParameters(x);
         }
     }
 
