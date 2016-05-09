53260ad06b74985437c34d1663d037d77536212c
boris-il-forte <boris.ilpossente@hotmail.it>
2016-05-09 12:53:35 +0200

    Added LQR test

---
+++ rele/test/IRL/nips/LQR.cpp
@@ -0,0 +1,164 @@
+/*
+ * rele,
+ *
+ *
+ * Copyright (C) 2016 Davide Tateo
+ * Versione 1.0
+ *
+ * This file is part of rele.
+ *
+ * rele is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rele is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rele.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * rele,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo & Matteo Pirotta
+ * Versione 1.0
+ *
+ * This file is part of rele.
+ *
+ * rele is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rele is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rele.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "rele/approximators/features/SparseFeatures.h"
+#include "rele/approximators/features/DenseFeatures.h"
+#include "rele/approximators/regressors/others/LinearApproximator.h"
+#include "rele/approximators/basis/IdentityBasis.h"
+
+#include "rele/policy/parametric/differentiable/LinearPolicy.h"
+#include "rele/statistics/DifferentiableNormals.h"
+
+#include "rele/environments/LQR.h"
+#include "rele/solvers/lqr/LQRsolver.h"
+#include "rele/core/PolicyEvalAgent.h"
+
+#include "rele/utils/FileManager.h"
+
+#include "rele/IRL/algorithms/EGIRL.h"
+
+#include "../RewardBasisLQR.h"
+
+using namespace std;
+using namespace arma;
+using namespace ReLe;
+
+int main(int argc, char *argv[])
+{
+	if(argc != 4)
+	{
+		cout << "Wrong argument number: dimension, n_episode, n_experiment must be provided" << endl;
+		return -1;
+	}
+
+	string dimension(argv[1]);
+	string n_episodes(argv[2]);
+	string n_experiment(argv[3]);
+
+    FileManager fm("nips/lqr_exact/" + dimension + "/" + n_episodes + "/" + n_experiment);
+    fm.createDir();
+    fm.cleanDir();
+    std::cout << std::setprecision(OS_PRECISION);
+
+    unsigned int dim = stoi(dimension);
+    unsigned int nbEpisodes = stoi(n_episodes);
+
+    //Set reward policy
+    arma::vec eReward;
+
+    if(dim == 2)
+    {
+    	eReward = {0.3, 0.7};
+    }
+    else
+    {
+    	eReward = {0.2, 0.7, 0.1};
+    }
+
+    int rewardDim = dim;
+    LQR mdp(dim, rewardDim);
+
+    BasisFunctions basis = IdentityBasis::generate(dim);
+
+    SparseFeatures phi;
+    phi.setDiagonal(basis);
+
+    DetLinearPolicy<DenseState> expertPolicy(phi);
+
+    // solve the problem in exact way
+    LQRsolver solver(mdp,phi);
+    solver.setRewardWeights(eReward);
+    mat K = solver.computeOptSolution();
+    arma::vec p = K.diag();
+    arma::mat Sigma = arma::eye(dim, dim);
+    Sigma *= 0.001;
+    ParametricNormal expertDist(p, Sigma);
+
+    std::cout << "Rewards: ";
+    for (int i = 0; i < eReward.n_elem; ++i)
+    {
+        std::cout << eReward(i) << " ";
+    }
+    std::cout << "| Params: " << expertDist.getParameters().t() << std::endl;
+
+
+    PolicyEvalDistribution<DenseAction, DenseState> expert(expertDist, expertPolicy);
+
+    // Generate LQR expert dataset
+    Core<DenseAction, DenseState> expertCore(mdp, expert);
+    CollectorStrategy<DenseAction, DenseState> collection;
+    expertCore.getSettings().loggerStrategy = &collection;
+    expertCore.getSettings().episodeLength = mdp.getSettings().horizon;
+    expertCore.getSettings().testEpisodeN = nbEpisodes;
+    expertCore.runTestEpisodes();
+    Dataset<DenseAction,DenseState>& data = collection.data;
+
+
+    // Create parametric reward
+    BasisFunctions basisReward;
+    for(unsigned int i = 0; i < eReward.n_elem; i++)
+        basisReward.push_back(new LQR_RewardBasis(i, dim));
+    DenseFeatures phiReward(basisReward);
+
+
+    LinearApproximator rewardRegressor(phiReward);
+
+    arma::mat theta = expert.getParams();
+    auto* irlAlg = new EGIRL<DenseAction, DenseState>(data, theta, expertDist,
+                rewardRegressor, mdp.getSettings().gamma, IrlEpGrad::PGPE_BASELINE);
+
+    //Run GIRL
+    irlAlg->run();
+    arma::vec omega = rewardRegressor.getParameters();
+
+    //Print results
+    cout << "Weights: " << omega.t();
+
+    //save weights
+    omega.save(fm.addPath("Weights.txt"),  arma::raw_ascii);
+
+    return 0;
+}
